ccd9460013a127bcc6c824cefff7a9a68bc68e19
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2022-12-05 10:22:56 +0100

    Add solution for puzzle 2 of day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -15,6 +15,10 @@ impl Section {
     fn contains(&self, other: Section) -> bool {
         other.low >= self.low && other.high <= self.high
     }
+
+    fn overlaps(&self, other: Section) -> bool {
+        self.low <= other.high && self.high >= other.low
+    }
 }
 
 fn main() -> Result<(), &'static str> {
@@ -30,6 +34,7 @@ fn main() -> Result<(), &'static str> {
     let lines = file_content.lines();
 
     let mut count_fully_contained = 0;
+    let mut count_overlapping = 0;
 
     for line in lines {
         let xs: Vec<_> = line
@@ -43,9 +48,14 @@ fn main() -> Result<(), &'static str> {
         if first_section.contains(second_section) || second_section.contains(first_section) {
             count_fully_contained += 1;
         }
+
+        if first_section.overlaps(second_section) {
+            count_overlapping += 1;
+        }
     }
 
     println!("Result of puzzle 1: {count_fully_contained}");
+    println!("Result of puzzle 2: {count_overlapping}");
 
     Ok(())
 }
